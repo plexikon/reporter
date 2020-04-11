631e7b477bd70b4c1922735980683ef46667c947
steph bug <plexikon@protonmail.com>
2020-04-11 15:55:25 +0200

    allow dispatching inner command

--- src/CommandPublisher.php
+++ src/CommandPublisher.php
@@ -4,11 +4,31 @@ declare(strict_types=1);
 namespace Plexikon\Reporter;
 
 use Plexikon\Reporter\Publisher\Publisher;
+use Throwable;
 
 class CommandPublisher extends Publisher
 {
+    private array $queue = [];
+    private bool $isDispatching = false;
+
     public function dispatch($message): void
     {
-        $this->dispatchMessage($message);
+        $this->queue[] = $message;
+
+        if (!$this->isDispatching) {
+            $this->isDispatching = true;
+
+            try {
+                while ($command = array_shift($this->queue)) {
+                    $this->dispatchMessage($command);
+                }
+            } catch (Throwable $exception) {
+                $this->isDispatching = false;
+
+                throw $exception;
+            }
+
+            $this->isDispatching = false;
+        }
     }
 }
